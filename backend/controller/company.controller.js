b8e0b509b7531224eb1b3625a942dd4ee16d144a
saurav-ghimire <sauravghimire0123@gmail.com>
2024-08-10 15:44:07 -0400

    Get company by id created

--- backend/controller/company.controller.js
+++ backend/controller/company.controller.js
@@ -37,3 +37,19 @@ export const getCompany = async (req,res) => {
   }
 }
 
+export const getCompanyById = async(req,res) => {
+try {
+  const companyId = req.params.id;
+  const company = await Company.findById(companyId);
+  if(!company){
+    return res.status(404).json({message:'Company Not Found', success:false}) 
+  }
+  
+  return res.status(200).json({company, success:true});
+
+} catch (error) {
+  console.log(error)
+}
+}
+
+
