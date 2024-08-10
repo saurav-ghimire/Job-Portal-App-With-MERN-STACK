06b87f4a12a98f2d2ea6a285538dadb469a85ef2
saurav-ghimire <sauravghimire0123@gmail.com>
2024-08-10 19:33:49 -0400

    Admin Jobs Route Created

--- backend/routes/jobs.route.js
+++ backend/routes/jobs.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getAllJobs, getJobsById, postJob } from "../controller/jobs.controller.js";
+import { getAdminJobs, getAllJobs, getJobsById, postJob } from "../controller/jobs.controller.js";
 import isAuthenticated from "../midllewares/isAuthenticated.js";
 
 const jobsRouter = express.Router();
@@ -7,4 +7,5 @@ const jobsRouter = express.Router();
 jobsRouter.post('/add', isAuthenticated, postJob)
 jobsRouter.get('/all', getAllJobs)
 jobsRouter.get('/:id', getJobsById)
+jobsRouter.get('/admin/jobs', isAuthenticated, getAdminJobs)
 export default jobsRouter;
