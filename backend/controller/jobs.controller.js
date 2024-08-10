23bbf653f8aa77d9c63a03cf36c1ff63974347da
saurav-ghimire <sauravghimire0123@gmail.com>
2024-08-10 19:05:53 -0400

    All Jobs controller for admin also created

--- backend/controller/jobs.controller.js
+++ backend/controller/jobs.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Jobs from "../models/jobs.model.js";
 
 export const postJob = async (req,res) => {
@@ -26,9 +27,49 @@ export const postJob = async (req,res) => {
 }
 export const getAllJobs = async(req,res)=> {
   try {
-    const jobs = await Jobs.find();
+    const keyword = req.query.keyword || "";
+    const query = {
+      $or:[
+        {title:{$regex:keyword,$options:"i"}},
+        {description:{$regex:keyword,$options:"i"}},
+      ]
+    }
+    const jobs = await Jobs.find(query);
+    if(!jobs){
+      return res.status(404).json({message:'Jobs Not Found', success:false});  
+    }
     return res.status(200).json({jobs, success:true});
   } catch (error) {
-    console.log(error)
+    console.log(error);
+  }
+}
+
+
+export const getJobsById = async(req,res) => {
+  try {
+    const id = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: 'Invalid Job ID', success: false });
+    }
+    const job = await Jobs.findById(id);
+    if(!job){
+      return res.status(404).json({ message: 'Job Not Found', success: false });
+    }
+    return res.status(200).json({ job, success: true });
+  } catch (error) {
+    console.log(error) 
+  }
+}
+
+export const getAdminJobs = async(req,res)=> {
+  try {
+    const adminId = req.id;
+    const jobs = await Jobs.find({created_by: adminId});
+    if(jobs.length < 1){
+      res.status(404).json({ message: 'Job Not Found', success: false });
+    }
+    res.status(200).json({jobs, success: false });
+  } catch (error) {
+    
   }
 }
