343682642fd45b91a396da9a9ce9746075c8c132
saurav-ghimire <sauravghimire0123@gmail.com>
2024-08-10 21:42:30 -0400

    Jobs Update controller and route created

--- backend/controller/jobs.controller.js
+++ backend/controller/jobs.controller.js
@@ -26,6 +26,31 @@ export const postJob = async (req,res) => {
     console.log(error)
   }
 }
+export const updateJob = async (req, res) => {
+  try {
+    const jobId = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(jobId)) {
+      return res.status(400).json({ message: 'Invalid Job ID', success: false });
+    }
+
+    const { title, description, requirements, salary, location, jobType, position, experience, companyId } = req.body;
+    const updateData = { title, description, requirements, salary, location, jobType, position, experience, companyId };
+
+    // Use `await` to get the updated job object
+    const job = await Jobs.findByIdAndUpdate(jobId, updateData, { new: true });
+
+    if (!job) {
+      return res.status(404).json({ message: 'Job Not Found', success: false });
+    }
+
+    return res.status(200).json({ message: 'Job Listing Updated', job, success: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
 export const getAllJobs = async(req,res)=> {
   try {
     const keyword = req.query.keyword || "";
--- backend/routes/jobs.route.js
+++ backend/routes/jobs.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getAdminJobs, getAllJobs, getJobsById, postJob } from "../controller/jobs.controller.js";
+import { getAdminJobs, getAllJobs, getJobsById, postJob, updateJob } from "../controller/jobs.controller.js";
 import isAuthenticated from "../midllewares/isAuthenticated.js";
 
 const jobsRouter = express.Router();
@@ -8,4 +8,5 @@ jobsRouter.post('/add', isAuthenticated, postJob)
 jobsRouter.get('/all', getAllJobs)
 jobsRouter.get('/:id', getJobsById)
 jobsRouter.get('/admin/jobs', isAuthenticated, getAdminJobs)
+jobsRouter.post('/:id', isAuthenticated, updateJob)
 export default jobsRouter;
